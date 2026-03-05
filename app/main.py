3e726745bfca8321560f66315cb03ea4b2c2e520
Minji <158cmwpdwkd@gmail.com>
2026-03-05 20:35:18 +0900

    제발 돌아가게해주세요

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from .routes_survey import router as survey_router
 from .routes_report import router as report_router
-from .routes_admin import router as admin_router
+from app.routes_admin import router as admin_router
 
 from .db import engine, Base
 from .models import UserSession, Order, PaidSurvey, Report, MessageSchedule  # 👈 중요 (등록용 import)
